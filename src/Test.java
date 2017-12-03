0c2d5799c969dbea91adf2fd26c4944babd96d87
guanth <2859000700@qq.com>
2017-12-03 15:45:58 +0800

    测试用户1提交

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(123);
+		System.out.println("测试用户1修改");
 		System.out.println("测试用户2提交");
+		System.out.println("测试用1提交");
 	}
 }
